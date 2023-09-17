e17d3a92b0fd23b176d5b75151cea87de2d0a8aa
Yazdani Chowdhury <yaz4dani@gmail.com>
2023-09-17 18:34:49 +0300

    WIP

--- controller/JobApplication.js
+++ controller/JobApplication.js
@@ -97,3 +97,33 @@ exports.deleteJobApplication = async (req, res) => {
     res.status(500).json({ error: 'Something went wrong' });
   }
 };
+
+/**
+ * To get all the job application list for a single job posts
+ * Only employer will have option see their own job applicant list
+ * This will first get a single job post details and then based on single job post,
+ * it will get all the job application list
+ * @param {*} req
+ * @param {*} res
+ * @returns
+ */
+exports.getJobDetailsApplicationList = async (req, res) => {
+  try {
+    const query = { slug: req.params.slug };
+    const singleJobDetails = await Job.findOne(query);
+
+    const logedInUser = req.user._id;
+    if (logedInUser !== singleJobDetails.postedBy._id.toString()) {
+      return res.status(422).json({ error: 'You cant view other employer job posts' });
+    }
+
+    const jobApplicationList = await JobApplication.find(
+      { jobPostId: singleJobDetails._id.toString() },
+      { status: 1, jobAppliedBy: 1, date: 1 }
+    ).populate('jobAppliedBy', 'name email slug role skills points accountType award');
+
+    res.status(200).json({ singleJobDetails, jobApplicationList });
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+};
--- router/JobApplication.js
+++ router/JobApplication.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 //Controller
 
-const { createJobApplication, getAllAppliedJobLists, deleteJobApplication } = require('../controller/JobApplication');
+const { createJobApplication, getAllAppliedJobLists, deleteJobApplication, getJobDetailsApplicationList } = require('../controller/JobApplication');
 
 //Middleware
 const { requireLogin, isAdmin, isEmployer } = require('../middleware/auth');
@@ -28,5 +28,14 @@ router.get('/get-applied-job-lists', requireLogin, getAllAppliedJobLists);
  */
 
 router.delete('/delete-job-application/:id', requireLogin, deleteJobApplication);
+
+/**
+ * To get all the job application list for a single job posts
+ * Only employer will have option see their own job applicant list
+ * This will first get a single job post details and then based on single job post,
+ * it will get all the job application list
+ */
+
+router.get('/job-details-applicationlist/:slug', requireLogin, isEmployer, getJobDetailsApplicationList);
 
 module.exports = router;
